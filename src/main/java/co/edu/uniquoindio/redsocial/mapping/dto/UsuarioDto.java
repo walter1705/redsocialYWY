6615fc6d424e69eeb6400aabaf349c13f38b0760
walter1705 <134169546+walter1705@users.noreply.github.com>
2024-09-29 00:15:47 -0500

    DTO idea

--- src/main/java/co/edu/uniquoindio/redsocial/mapping/dto/UsuarioDto.java
+++ src/main/java/co/edu/uniquoindio/redsocial/mapping/dto/UsuarioDto.java
@@ -2,9 +2,8 @@ package co.edu.uniquoindio.redsocial.mapping.dto;
 
 import co.edu.uniquoindio.redsocial.model.Usuario;
 
-public record VendedorDto(
-        String nombre,
-        String apellido,
-        String email
+public record UsuarioDto(
+        String username,
+        String password
 ) {
 }
---
+++ src/main/java/co/edu/uniquoindio/redsocial/mapping/dto/UsuarioVendedorDto.java
@@ -0,0 +1,7 @@
+package co.edu.uniquoindio.redsocial.mapping.dto;
+
+public record UsuarioVendedorDto(
+        VendedorDto vendedorDto,
+        UsuarioDto usuarioDto
+) {
+}
