88fe61312e3c7f515bf775be28d81ec82390f2c1
walter1705 <134169546+walter1705@users.noreply.github.com>
2024-11-11 13:19:02 -0500

    avance vista vendedor dinamico
    Co-Authored-By: yullvanegas <151601768+yullvanegas@users.noreply.github.com>

---
+++ src/main/java/co/edu/uniquoindio/redsocial/viewController/VendedorTemplateViewController.java
@@ -0,0 +1,11 @@
+package co.edu.uniquoindio.redsocial.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class VendedorTemplateViewController {
+
+    @FXML
+    private Label likeCounter;
+
+}
