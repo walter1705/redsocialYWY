9f197da4020ed8c1fedde7d7983a47a3c9529a14
walter1705 <134169546+walter1705@users.noreply.github.com>
2024-11-12 17:17:22 -0500

    Vendedor template view advance
    Co-Authored-By: yullvanegas <151601768+yullvanegas@users.noreply.github.com>

--- src/main/java/co/edu/uniquoindio/redsocial/viewController/VendedorTemplateViewController.java
+++ src/main/java/co/edu/uniquoindio/redsocial/viewController/VendedorTemplateViewController.java
@@ -2,11 +2,13 @@ package co.edu.uniquoindio.redsocial.viewController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import co.edu.uniquoindio.redsocial.model.EstadoProducto;
+import co.edu.uniquoindio.redsocial.model.Producto;
+import co.edu.uniquoindio.redsocial.model.Vendedor;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 public class VendedorTemplateViewController {
@@ -18,7 +20,25 @@ public class VendedorTemplateViewController {
     private URL location;
 
     @FXML
+    private ImageView ImageViewProductCrud;
+
+    @FXML
+    private Button bttnActualizarProducto;
+
+    @FXML
+    private Button bttnCrearProducto;
+
+    @FXML
+    private Button bttnEliminarProducto;
+
+    @FXML
+    private Button bttnImagenProductoCrud;
+
+    @FXML
     private ToggleButton bttnLike;
+
+    @FXML
+    private ComboBox<EstadoProducto> comboBoxEstado;
 
     @FXML
     private ImageView imageViewListProductos;
@@ -27,19 +47,52 @@ public class VendedorTemplateViewController {
     private Label likeCounter;
 
     @FXML
-    private TableColumn<?, ?> tableViewListaContactos;
+    private TableView<Vendedor> tableViewListaContactos;
 
     @FXML
-    private TableColumn<?, ?> tcProductCategoria;
+    private TableView<Producto> tableViewPublicados;
 
     @FXML
-    private TableColumn<?, ?> tcProductDescripcion;
+    private TableColumn<Vendedor, String> tcViewListaContactos;
 
     @FXML
-    private TableColumn<?, ?> tcProductNombre;
+    private TableColumn<Vendedor, String> tcCategoriaProductoPro;
 
     @FXML
-    private TableColumn<?, ?> tcProductPrecio;
+    private TableColumn<Vendedor, String> tcDescripcionProductoPro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcEstadoProductoPro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcNombreProductoPro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcPrecioProductoPro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcProductCategoriaMuro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcProductDescripcionMuro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcProductNombreMuro;
+
+    @FXML
+    private TableColumn<Vendedor, String> tcProductPrecioMuro;
+
+    @FXML
+    private TextField txtCategoriaProducto;
+
+    @FXML
+    private TextField txtDescripcionProducto;
+
+    @FXML
+    private TextField txtNombreProducto;
+
+    @FXML
+    private TextField txtPrecioProducto;
 
     @FXML
     void onBttnLike(ActionEvent event) {
@@ -47,15 +100,54 @@ public class VendedorTemplateViewController {
     }
 
     @FXML
+    void onImagenProductoPath(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActualizarProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCrearProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEliminarProducto(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        comboBoxEstado.getItems().addAll(EstadoProducto.values());
+
+
+
+        assert ImageViewProductCrud != null : "fx:id=\"ImageViewProductCrud\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert bttnActualizarProducto != null : "fx:id=\"bttnActualizarProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert bttnCrearProducto != null : "fx:id=\"bttnCrearProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert bttnEliminarProducto != null : "fx:id=\"bttnEliminarProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert bttnImagenProductoCrud != null : "fx:id=\"bttnImagenProductoCrud\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
         assert bttnLike != null : "fx:id=\"bttnLike\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert comboBoxEstado != null : "fx:id=\"comboBoxEstado\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
         assert imageViewListProductos != null : "fx:id=\"imageViewListProductos\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
         assert likeCounter != null : "fx:id=\"likeCounter\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
         assert tableViewListaContactos != null : "fx:id=\"tableViewListaContactos\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
-        assert tcProductCategoria != null : "fx:id=\"tcProductCategoria\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
-        assert tcProductDescripcion != null : "fx:id=\"tcProductDescripcion\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
-        assert tcProductNombre != null : "fx:id=\"tcProductNombre\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
-        assert tcProductPrecio != null : "fx:id=\"tcProductPrecio\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcCategoriaProductoPro != null : "fx:id=\"tcCategoriaProductoPro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcDescripcionProductoPro != null : "fx:id=\"tcDescripcionProductoPro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcEstadoProductoPro != null : "fx:id=\"tcEstadoProductoPro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcNombreProductoPro != null : "fx:id=\"tcNombreProductoPro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcPrecioProductoPro != null : "fx:id=\"tcPrecioProductoPro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcProductCategoriaMuro != null : "fx:id=\"tcProductCategoriaMuro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcProductDescripcionMuro != null : "fx:id=\"tcProductDescripcionMuro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcProductNombreMuro != null : "fx:id=\"tcProductNombreMuro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert tcProductPrecioMuro != null : "fx:id=\"tcProductPrecioMuro\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert txtCategoriaProducto != null : "fx:id=\"txtCategoriaProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert txtDescripcionProducto != null : "fx:id=\"txtDescripcionProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert txtNombreProducto != null : "fx:id=\"txtNombreProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
+        assert txtPrecioProducto != null : "fx:id=\"txtPrecioProducto\" was not injected: check your FXML file 'VendedorTemplate.fxml'.";
 
     }
 
